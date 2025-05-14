98f178160d073c91c4ccf684e00dde6489a94802
avraham lashansky <165169428+avelash@users.noreply.github.com>
2025-05-14 12:04:55 +0300

    Update log.ts

--- log.ts
+++ log.ts
@@ -1,24 +1,82 @@
 import winston from "winston";
 import Transport from "datadog-winston";
-import { getDatadogApiKey } from "./genericEnvVar";
+import { getDatadogApiKey, getEnv } from "./genericEnvVar";
+import os from "os";
+import { debug } from "console";
+
+const environment = getEnv();
+//const envsToLog = new Set(["test", "stag", "prod"]);
 
 const logger = winston.createLogger({
+  level: "info",
+  defaultMeta: { env: environment },
   transports: [
-    new winston.transports.Console(),
-    new Transport({ apiKey: getDatadogApiKey() }),
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    }),
+
+    new Transport({
+      apiKey: getDatadogApiKey(),
+      format: winston.format.json(),
+    }),
   ],
 });
 
-function logInfo(message: string) {
-  logger.info(message);
+function sendToDatadog(): boolean {
+  return process.env.DEBUG === "false";
 }
 
-function logWarn(message: string) {
-  logger.warn(message);
+function logExtraParams() {
+  const server = process.env.serverName;
+  return { server };
 }
 
-function logError(message: string, e: Error) {
-  logger.error(message, e);
+function logInfo(...message: string[]) {
+  const logMessage = message.join(",");
+  if (sendToDatadog()) {
+    logger.info(logMessage, {
+      ...logExtraParams(),
+    });
+  } else {
+    console.log(logMessage, {
+      status: "info",
+      ...logExtraParams(),
+    });
+  }
+}
+
+function logWarn(...message: string[]) {
+  const logMessage = message.join(",");
+  if (sendToDatadog()) {
+    logger.warn(logMessage, {
+      ...logExtraParams(),
+    });
+  } else {
+    console.log(logMessage, {
+      status: "warn",
+      ...logExtraParams(),
+    });
+  }
+}
+
+function logError(e: Error, ...message: string[]) {
+  const logMessage = message.join(",");
+  if (sendToDatadog()) {
+    logger.error(logMessage, {
+      error: e.message,
+      stack: e.stack,
+      ...logExtraParams(),
+    });
+  } else {
+    console.log(logMessage, {
+      status: "error",
+      error: e.message,
+      stack: e.stack,
+    });
+  }
 }
 
 export { logInfo, logWarn, logError };
